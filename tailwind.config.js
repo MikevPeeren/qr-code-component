beb71c9a6eb9f210551e4a801ca73c9d99d6829d
MikevPeeren <mikevpeeren@hotmail.com>
2022-01-22 18:12:10 +0100

    :bug: fixed bug with color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       colors: {
         "light-gray": "#F2F2F2",
         gray: "#7D889E",
-        blue: "##2C7DFA",
+        blue: "#2C7DFA",
         "blue-shade": "#3685FF",
         "dark-navy": "#1F314F",
       },
