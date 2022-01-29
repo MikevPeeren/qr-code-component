596363236c7a6a33f90ab11e4392fd5d18df3de5
MikevPeeren <mikevpeeren@hotmail.com>
2022-01-29 16:52:48 +0100

    :recycle: added landmarks

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <div className="bg-white shadow-2xl w-10/12 md:w-5/12 lg:w-4/12 xl:w-3/12 2xl:w-3/12 flex flex-col justify-center items-center rounded-2xl px-4">
-          <div className="my-4 p-4 bg-blue w-full rounded-2xl">
-            <div className="flex items-center justify-center">
-              <QRCode className={`w-full h-full fill-white`} />
+      <main>
+        <div className="min-h-screen flex flex-col justify-center items-center">
+          <div className="bg-white shadow-2xl w-10/12 md:w-5/12 lg:w-4/12 xl:w-3/12 2xl:w-3/12 flex flex-col justify-center items-center rounded-2xl px-4">
+            <div className="my-4 p-4 bg-blue w-full rounded-2xl">
+              <div className="flex items-center justify-center">
+                <QRCode className={`w-full h-full fill-white`} />
+              </div>
             </div>
+            <h1 className="mt-2 font-bold text-2xl text-dark-navy text-center text-focus-in-first">
+              Improve your front-end skills by building projects
+            </h1>
+            <h2 className="mt-4 mb-10 font-normal text-base text-gray text-center text-focus-in-second">
+              Scan the QR code to visit Mike van Peeren&apos;s website and take
+              your coding skills to the next level by contacting him!
+            </h2>
           </div>
-          <h1 className="mt-2 font-bold text-2xl text-dark-navy text-center text-focus-in-first">
-            Improve your front-end skills by building projects
-          </h1>
-          <h2 className="mt-4 mb-10 font-normal text-base text-gray text-center text-focus-in-second">
-            Scan the QR code to visit Mike van Peeren&apos;s website and take
-            your coding skills to the next level by contacting him!
-          </h2>
         </div>
-      </div>
+      </main>
+      <footer></footer>
     </div>
   );
 }
